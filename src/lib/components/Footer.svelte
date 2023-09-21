700dc5b28de414a46f1df93215726997da606d10
João Nogueira <jrnogueira@edu.ulisboa.pt>
2023-09-21 14:45:16 +0100

    Update logos

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -7,8 +7,6 @@
 	<img src="{base}/logos/cerena.svg" alt="CERENA" class="h-10" />
 	<img src="{base}/logos/iti.svg" alt="ITI" class="h-10" />
 	<img src="{base}/logos/cegist.svg" alt="CEGIST" class="h-10" />
-	<img src="{base}/logos/fct.svg" alt="FCT" class="h-10" />
-	<img src="{base}/logos/dgs.png" alt="DGS" class="h-10" />
 	<img src="{base}/logos/insa.png" alt="INSA" class="h-10" />
-	<img src="{base}/logos/ipst.png" alt="IPST" class="h-10" />
+	<img src="{base}/logos/fct.svg" alt="FCT" class="h-10" />
 </footer>
