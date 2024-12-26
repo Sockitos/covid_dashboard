d28bad9f4da299a45fdfec10f5016b7b398e4e87
carolina2300 <carolinanmarcal@gmail.com>
2024-12-26 00:37:33 +0000

    Hot reloading & footer improvements

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,6 +2,33 @@
 	import { base } from '$app/paths';
 </script>
 
+<style>
+    @media (max-width: 1250px) {
+        footer {
+            gap: 5rem;
+        }
+        footer img {
+            height: 1.5rem;
+        }
+    }
+    @media (max-width: 920px) {
+        footer {
+            gap: 2rem;
+        }
+        footer img {
+            height: 1.5rem;
+        }
+    }
+    @media (max-width: 640px) {
+        footer {
+            gap: 2rem;
+        }
+        footer img {
+            height: 1.5rem;
+        }
+		}
+</style>
+
 <footer class="bg-white flex flex-row flex-wrap gap-20 p-5 items-center justify-center">
 	<img src="{base}/logos/ist.svg" alt="IST" class="h-10" />
 	<img src="{base}/logos/cerena.svg" alt="CERENA" class="h-10" />
